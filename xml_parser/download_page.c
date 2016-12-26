a86e562b6cf9456e9f851ce4be726bb2a04dabd9
Eric P <congpeng89@gmail.com>
2016-12-25 23:38:02 -0600

    Tests for everything.
    Everything works as expected!

---
+++ xml_parser/download_page.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+ 
+#include <curl/curl.h>
+ 
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+    return written;
+}
+ 
+int main()
+{
+    CURL *curl_handle;
+    static const char *pagefilename = "page.out";
+    FILE *pagefile;
+ 
+    // if(argc < 2) 
+    // {
+    //     printf("Usage: %s <URL>\n", argv[0]);
+    //     return 1;
+    // }
+ 
+    curl_global_init(CURL_GLOBAL_ALL);
+ 
+    /* init the curl session */ 
+    curl_handle = curl_easy_init();
+
+    if(!curl_handle)
+    {
+        printf("Error in initializtin curl!\n");
+        return -1;
+    }
+ 
+    /* set URL to get here */ 
+    curl_easy_setopt(curl_handle, CURLOPT_URL, 
+            "https://en.wikipedia.org/wiki/University_of_Illinois_at_Urbana-Champaign");
+     
+    /* Switch on full protocol/debug output while testing */ 
+    curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+    
+    /* disable progress meter, set to 0L to enable and disable debug output */ 
+    curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
+     
+    /* send all data to this function  */ 
+    curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+ 
+    /* open the file */ 
+    pagefile = fopen(pagefilename, "wb");
+    if(pagefile) 
+    {
+        /* write the page body to this file handle */ 
+        curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, pagefile);
+     
+        /* get it! */ 
+        curl_easy_perform(curl_handle);
+     
+        /* close the header file */ 
+        fclose(pagefile);
+    }
+ 
+    /* cleanup curl stuff */ 
+    curl_easy_cleanup(curl_handle);
+ 
+    return 0;
+}
--- xml_parser/xml_parser.c
+++ xml_parser/xml_parser.c
@@ -6,6 +6,8 @@ int main()
 	CURL *curl;
 	CURLcode res;
 
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+
 	curl = curl_easy_init();
 	if(curl)
 	{
@@ -13,16 +15,17 @@ int main()
 			"https://en.wikipedia.org/wiki/University_of_Illinois_at_Urbana-Champaign");
 
 		// the following line tells to follow the redirect
-		// curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
 		res = curl_easy_perform(curl);
 
 		/* Check for errors */ 
     	if(res != CURLE_OK)
-      		fprintf(stderr, "curl_easy_perform() failed: %s\n",
-        curl_easy_strerror(res));
+      		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
  
     	/* always cleanup */ 
     	curl_easy_cleanup(curl);	
 	}
+	curl_global_cleanup();
+	return 0;
 }
